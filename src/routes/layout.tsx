915ca31d0eb15b3a95d94edf8ecdea2b628de010
Julian Goacher <julian.goacher@innerfunction.com>
2024-08-27 13:31:42 +0100

    Style improvements

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,9 @@
 import { component$, Slot } from "@builder.io/qwik";
 import type { RequestHandler } from "@builder.io/qwik-city";
+import { useContent, useDocumentHead } from "@builder.io/qwik-city";
+import type { ContentHeading, DocumentMeta } from "@builder.io/qwik-city"; 
+
+import logo from './pagedrop-logo.svg'
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   // Control caching for this request for best performance and to reduce hosting costs:
@@ -12,6 +16,73 @@ export const onGet: RequestHandler = async ({ cacheControl }) => {
   });
 };
 
+type PageInfo = {
+  title: string,
+  meta?: readonly DocumentMeta[]
+  headings?: ContentHeading[]
+}
+
+type SectionProps = {
+  pageInfo: PageInfo
+}
+
+const readMeta = (pageInfo: PageInfo, name: string, defaultValue: string = '') => {
+  const { meta } = pageInfo
+  const found = meta && meta.find(item => item.name === name)
+  return found ? found.content : defaultValue
+}
+
+const styles = {
+  header: {
+    fontSize: '2em',
+    fontWeight: 'bold',
+    color: 'white',
+    backgroundColor: 'black',
+    padding: '0.5em 0.5em 0.5em 0.5em'
+  },
+  logo: {
+    marginRight: '0.5em'
+  },
+  body: {
+    margin: '1em',
+    fontFamily: 'Georgia, serif',
+    lineHeight: '140%'
+  },
+  footer: {
+    fontSize: '0.8em',
+    fontWeight: 'normal',
+    color: 'white',
+    backgroundColor: 'black',
+    padding: '1em'
+  }
+}
+
+const Header = ({ pageInfo }: SectionProps) => {
+  return (
+    <div style={styles.header}>
+      <img width={50} height={50} style={styles.logo} src={logo} align="center" />
+      {pageInfo.title}
+    </div>
+  )
+}
+
+const Footer = ({ pageInfo }: SectionProps) => {
+  return (
+    <div style={styles.footer}>{readMeta(pageInfo, 'footerText', 'Pagedrop')}</div>
+  )
+}
+
 export default component$(() => {
-  return <Slot />;
+  const { title, meta } = useDocumentHead()
+  const { headings } = useContent()
+  const pageInfo = { title, meta, headings }
+  return (
+    <>
+      <Header pageInfo={pageInfo} />
+      <div style={styles.body}>
+        <Slot />
+      </div>
+      <Footer pageInfo={pageInfo} />
+    </>
+  )
 });
